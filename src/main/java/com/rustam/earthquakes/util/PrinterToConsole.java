c39a28910bf3a27f0f33d5882f2ee64a237a58f7
rustam <rustam.kamberov@gmail.com>
2020-12-27 14:22:33 +0100

    time/date added to the console print-out

--- src/main/java/com/rustam/earthquakes/util/PrinterToConsole.java
+++ src/main/java/com/rustam/earthquakes/util/PrinterToConsole.java
@@ -3,15 +3,18 @@ package com.rustam.earthquakes.util;
 import com.rustam.earthquakes.model.Earthquake;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
 public class PrinterToConsole implements IPrinterToConsole{
     @Override
     public void print(List<Earthquake> earthquakes) {
-        /**I am aware of the smelly code due to printing to console
-         *
-         * I could have used slf4j.Logger but the output is not copy/paste friendly for testing*/
+        /* I am aware of the smelly code due to printing to console
+         * I could have used slf4j.Logger but the output is not copy/paste friendly for testing */
+        System.out.println("Observations accessed on " + LocalDate.now() + ", at "
+                + LocalTime.now().getHour() + ":" + LocalTime.now().getMinute() + ":" + LocalTime.now().getSecond());
         for (Earthquake e : earthquakes) {
             System.out.println(e.getTitle() + " || " + e.getDistance());
         }
