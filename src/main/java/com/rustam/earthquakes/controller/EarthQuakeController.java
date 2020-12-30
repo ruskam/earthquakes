3ada777507c5add318da57d3670e17e3c74e40ee
rustam <rustam.kamberov@gmail.com>
2020-12-30 10:42:58 +0100

    added a wrapper object for Earthquake to return the object rather than an a list

--- src/main/java/com/rustam/earthquakes/controller/EarthQuakeController.java
+++ src/main/java/com/rustam/earthquakes/controller/EarthQuakeController.java
@@ -2,6 +2,8 @@ package com.rustam.earthquakes.controller;
 
 import com.rustam.earthquakes.exception.UsgsResourceNotFoundException;
 import com.rustam.earthquakes.model.Earthquake;
+import com.rustam.earthquakes.model.EarthquakeWrapper;
+import com.rustam.earthquakes.model.UsgsResponse;
 import com.rustam.earthquakes.service.IUsgsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -24,14 +27,32 @@ public class EarthQuakeController {
     }
 
     @GetMapping(value = "/lat/{lat}/lon/{lon}")
-    public ResponseEntity<List<Earthquake>> getEQ(@PathVariable("lat") double lat, @PathVariable("lon") double lon) {
+    public ResponseEntity<EarthquakeWrapper> getEQ(@PathVariable("lat") double lat, @PathVariable("lon") double lon) {
         try {
             List<Earthquake> earthquakes = service.getEarthquake(lat, lon);
-            return ResponseEntity.ok().body(earthquakes);
+
+            EarthquakeWrapper earthquakeWrapper = new EarthquakeWrapper();
+            earthquakeWrapper.setEarthquakes(earthquakes);
+//            return ResponseEntity.ok().body(earthquakes);
+            return ResponseEntity.ok().body(earthquakeWrapper);
         } catch (UsgsResourceNotFoundException e) {
             throw new ResponseStatusException(
                 HttpStatus.NOT_FOUND, "something went wrong for location" +
                 lat + ", " + lon, e);
+        }
+    }
+
+    @GetMapping(value = "/async/lat/{lat}/lon/{lon}")
+    public Mono<UsgsResponse> getEQAsync(@PathVariable("lat") double lat, @PathVariable("lon") double lon) {
+        try {
+            Mono<UsgsResponse> earthquakeAsync = service.getEarthquakeAsync(lat, lon);
+//            return ResponseEntity.ok().body(earthquakeAsync);
+                        return earthquakeAsync;
+
+        } catch (UsgsResourceNotFoundException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "something went wrong for location" +
+                    lat + ", " + lon, e);
         }
     }
 
