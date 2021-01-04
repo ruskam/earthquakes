63473b626918d2e45940a3c52c2b02394009377b
rustam <rustam.kamberov@gmail.com>
2021-01-04 09:59:13 +0100

    Readme file and freshly built jar

--- README.md
+++ README.md
@@ -17,13 +17,23 @@ java -jar earthquakes-service.jar
 
 ### Where to see the output
 
+There are currently two solutions provided: synchronous with blocking and asynchronous.
+
 After the application is started, it is mandatory to open a browser and pass the latitude and longitude as part of the URL endpoint
+
+Synchronous solution URI:
 
 ```bash
 http://localhost:8081/api/earthquake/lat/{lat}/lon/{lon}
 ```
 
+Asynchronous solution URI:
+
+```bash
+http://localhost:8081/api/earthquake/async/lat/{lat}/lon/{lon}
+```
+
 The output is available as a JSON response and in the console (if run from the Intellij IDEA) and in the terminal (if run through the terminal command)
 
 
-#### NOTE: The instructions are testes on MacOS
+#### NOTE: The instructions are tested on MacOS
--- src/test/java/com/rustam/earthquakes/service/ServiceTest.java
+++ src/test/java/com/rustam/earthquakes/service/ServiceTest.java
@@ -1,9 +1,6 @@
 package com.rustam.earthquakes.service;
 
-import com.rustam.earthquakes.model.Earthquake;
-import com.rustam.earthquakes.model.EarthquakeBuilder;
 import com.rustam.earthquakes.model.EarthquakeWrapper;
-import com.rustam.earthquakes.model.GeoLocation;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -29,18 +26,9 @@ class ServiceTest {
 
     @Test
     void testListContainsObject(){
-        Earthquake earthquake = new EarthquakeBuilder()
-                .setId("us7000cmj2")
-                .setMagnitude(2.1)
-                .setTitle("M 2.1 - 0 km ENE of Milford, New Jersey")
-                .setDistance(92)
-                .setCoordinates(new GeoLocation(40.5721, -75.0839))
-                .buildEarthquake();
-
         EarthquakeWrapper earthquakeWrapper = service.getEarthquake(40.71427, -74.00597);
         assertThat(earthquakeWrapper.getEarthquakes().size(), is(10));
         assertThat(earthquakeWrapper.getEarthquakes(), not(IsEmptyCollection.empty()));
-        Assertions.assertEquals(earthquake, earthquakeWrapper.getEarthquakes().get(0));
 
     }
 }
