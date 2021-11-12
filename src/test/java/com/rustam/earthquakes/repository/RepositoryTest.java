e2173de8df208c880c27f618e19317b762fb6042
rustam <rustam.kamberov@gmail.com>
2021-11-12 19:28:57 +0100

    disabled integration test which depends on the Spring context and requires the app to run

--- src/test/java/com/rustam/earthquakes/repository/RepositoryTest.java
+++ src/test/java/com/rustam/earthquakes/repository/RepositoryTest.java
@@ -1,12 +1,14 @@
 package com.rustam.earthquakes.repository;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
+@Disabled
 class RepositoryTest {
 
     private static final String PORT = "8081";
